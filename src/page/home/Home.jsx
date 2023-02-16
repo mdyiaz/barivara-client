8b90c50559df3a4582d1ec14a1bbab90f90b9baf
mdyiaz <mdyiaz@gmail.com>
2023-02-16 12:55:59 +0600

    categorys

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import OurCategory from "../../component/OurCategory/OurCategory";
 import Slider from "../../component/Slider/Slider";
 
 function Home() {
   return (
     <div>
       <Slider></Slider>
+      <OurCategory></OurCategory>
     </div>
   );
 }
