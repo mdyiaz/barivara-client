77748d5e30cb45d99baba9465829b287b5a1bcfd
Md Masud Rana <mdmasude7@gmail.com>
2023-02-16 13:52:30 +0600

    asdf

--- src/component/Testimonial/Testimonial.jsx
+++ src/component/Testimonial/Testimonial.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Testimonial = () => {
   return (
-    <div>
+    <div className=" container mx-auto">
       <h2>Testimonial</h2>
     </div>
   );
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeCard from "../../component/HomeCard/HomeCard";
 import OurCategory from "../../component/OurCategory/OurCategory";
+import PhotoGellary from "../../component/PhotoGellary/PhotoGellary";
 import Slider from "../../component/Slider/Slider";
 import Testimonial from "../../component/Testimonial/Testimonial";
 
@@ -10,7 +11,7 @@ function Home() {
       <Slider></Slider>
       <OurCategory></OurCategory>
       <HomeCard></HomeCard>
-
+      <PhotoGellary></PhotoGellary>
       <Testimonial></Testimonial>
     </div>
   );
