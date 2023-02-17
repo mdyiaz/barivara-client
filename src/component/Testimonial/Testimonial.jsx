8aedf07d28a18f65f2a9a9d142161097a28db885
Md Masud Rana <mdmasude7@gmail.com>
2023-02-17 22:21:09 +0600

    slider

--- src/component/Testimonial/Testimonial.jsx
+++ src/component/Testimonial/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,7 +9,7 @@ import "swiper/css/pagination";
 // import "./styles.css";
 
 // import required modules
-import { Pagination } from "swiper";
+import { Autoplay, Pagination } from "swiper";
 import { useQuery } from "@tanstack/react-query";
 
 const Testimonial = () => {
@@ -26,8 +26,15 @@ const Testimonial = () => {
     <div className=" container mx-auto mt-10">
       <>
         <Swiper
+          loop={true}
           slidesPerView={3}
           spaceBetween={30}
+          // automatically
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: true,
+          }}
+          //
           pagination={{
             clickable: true,
           }}
@@ -45,13 +52,20 @@ const Testimonial = () => {
               spaceBetween: 50,
             },
           }}
-          modules={[Pagination]}
+          modules={[Pagination, Autoplay]}
           className="mySwiper"
         >
           <div className=""></div>
           {categorys.map((category) => (
             <SwiperSlide>
-              <div className="bg-primary text-white p-4 mb-10">
+              <div className="bg-primary mt-8 testimonial text-white p-4 mb-10 rounded-lg">
+                <div className="">
+                  <img
+                    className="w-[100px] h-[100px] rounded-full"
+                    src="https://images.unsplash.com/photo-1611548224889-08f2aa1a88f8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c21hcnQlMjBib3l8ZW58MHx8MHx8&w=1000&q=80"
+                    alt=""
+                  />
+                </div>
                 <h2 className="text-2xl font-semibold">Md Masud Rana</h2>
                 <p>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
