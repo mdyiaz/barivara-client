81a3bc4e8fa80aa898bc5b99e1cb9ae5e8e89768
Md Masud Rana <mdmasude7@gmail.com>
2023-02-19 12:05:57 +0600

    dashbord

--- src/routes/mainRoutes.js
+++ src/routes/mainRoutes.js
@@ -1,38 +1,43 @@
-import About from '../component/About/About'
-import Contact from '../component/Contact/Contact'
-import SignUp from '../page/login/SignUp'
-import Login from './../page/login/Login'
-const { createBrowserRouter } = require('react-router-dom')
-const { default: MainLayout } = require('../layout/main/MainLayout')
-const { default: Home } = require('../page/home/Home')
+import About from "../component/About/About";
+import Contact from "../component/Contact/Contact";
+import Dashbord from "../layout/Dashbord";
+import SignUp from "../page/login/SignUp";
+import Login from "./../page/login/Login";
+const { createBrowserRouter } = require("react-router-dom");
+const { default: MainLayout } = require("../layout/main/MainLayout");
+const { default: Home } = require("../page/home/Home");
 
 const routes = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <MainLayout />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
       },
       {
-        path: '/sign-up',
+        path: "/sign-up",
         element: <SignUp />,
       },
       {
-        path: '/contactus',
-        element:<Contact></Contact>
+        path: "/contactus",
+        element: <Contact></Contact>,
       },
       {
-        path:'/aboutus',
-        element:<About></About>
-      }
+        path: "/aboutus",
+        element: <About></About>,
+      },
     ],
   },
-])
+  {
+    path: "/dashbord",
+    element: <Dashbord></Dashbord>,
+  },
+]);
 
-export default routes
+export default routes;
