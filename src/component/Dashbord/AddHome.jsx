4f980832b19fc39ca76f688bffe7d10dd2709478
Md Masud Rana <mdmasude7@gmail.com>
2023-02-21 13:30:42 +0600

    add product

--- src/component/Dashbord/AddHome.jsx
+++ src/component/Dashbord/AddHome.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import React from "react";
 
 const AddHome = () => {
-  const [Gas, setGas] = useState(false);
-  console.log(Gas);
+  const addHomeHandler = (event) => {
+    event.preventDefault();
+    console.log("object");
+  };
   return (
     <div className="px-4">
       <div
@@ -11,7 +13,7 @@ const AddHome = () => {
         <h1 className="  mb-4 text-3xl text-primary font-semibold">
           Add Your Home
         </h1>
-        <form>
+        <form onSubmit={addHomeHandler}>
           <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
             <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
               <label htmlFor="f_name" className="text-xl font-medium">
@@ -32,7 +34,7 @@ const AddHome = () => {
               <input
                 type="number"
                 placeholder="Enter Home Price"
-                name="last_name"
+                name="price"
                 className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
               />
             </div>
@@ -43,7 +45,7 @@ const AddHome = () => {
               Location
             </label>
             <input
-              type="email"
+              type="text"
               placeholder="Enter Your Location"
               name="location"
               className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
@@ -69,7 +71,7 @@ const AddHome = () => {
               <input
                 type="file"
                 placeholder="Enter Your Password"
-                name="password"
+                name="photo"
                 className="file-input file-input-bordered file-input-primary w-full max-w-xs"
               />
             </div>
@@ -77,8 +79,13 @@ const AddHome = () => {
               <label htmlFor="email" className="text-xl font-medium">
                 Select Cetagory
               </label>
-              <select className="select w-full max-w-xs  select-bordered">
+              <select
+                name="Cetagory"
+                className="select w-full max-w-xs  select-bordered"
+              >
                 <option value="Homer">Home</option>
+                <option value="helo">helo</option>
+                <option value="masud">masud</option>
               </select>
             </div>
           </div>
@@ -88,7 +95,10 @@ const AddHome = () => {
               <label htmlFor="email" className="text-xl font-medium">
                 BedRoom
               </label>
-              <select className="select w-full max-w-xs  select-bordered">
+              <select
+                name="bed"
+                className="select w-full max-w-xs  select-bordered"
+              >
                 <option value={1}>1</option>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
@@ -102,7 +112,10 @@ const AddHome = () => {
               <label htmlFor="email" className="text-xl font-medium">
                 BathRoom
               </label>
-              <select className="select w-full max-w-xs  select-bordered">
+              <select
+                name="bath"
+                className="select w-full max-w-xs  select-bordered"
+              >
                 <option value={1}>1</option>
                 <option value={2}>2</option>
                 <option value={3}>3</option>
@@ -121,21 +134,13 @@ const AddHome = () => {
               <label htmlFor="email" className="text-xl font-medium">
                 Gas
               </label>
-              <input
-                onChange={() => setGas(true)}
-                type="checkbox"
-                className="checkbox"
-              />
+              <input name="gas" type="checkbox" className="checkbox" />
             </div>
             <div className=" rounded-lg flex items-center gap-2 mb-4  ">
               <label htmlFor="email" className="text-xl font-medium">
-                Water
+                Watter
               </label>
-              <input
-                onChange={() => setGas(true)}
-                type="checkbox"
-                className="checkbox"
-              />
+              <input name="watter" type="checkbox" className="checkbox" />
             </div>
           </div>
 
@@ -144,21 +149,13 @@ const AddHome = () => {
               <label htmlFor="email" className="text-xl font-medium">
                 Electricity
               </label>
-              <input
-                onChange={() => setGas(true)}
-                type="checkbox"
-                className="checkbox"
-              />
+              <input name="electricity" type="checkbox" className="checkbox" />
             </div>
             <div className=" rounded-lg flex items-center gap-2 mb-4  ">
               <label htmlFor="email" className="text-xl font-medium">
                 Internet
               </label>
-              <input
-                onChange={() => setGas(true)}
-                type="checkbox"
-                className="checkbox"
-              />
+              <input name="internet" type="checkbox" className="checkbox" />
             </div>
           </div>
 
@@ -171,7 +168,7 @@ const AddHome = () => {
             <textarea
               type="text"
               placeholder="Enter Your Home description  "
-              name="location"
+              name="description"
               className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
             />
           </div>
