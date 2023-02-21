602921e3e09db0f96a0274355414817157d19b3c
Md Masud Rana <mdmasude7@gmail.com>
2023-02-21 12:57:04 +0600

    check

--- src/page/Seller/SellerRequest/SellerRequest.js
+++ src/page/Seller/SellerRequest/SellerRequest.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
 const SellerRequest = () => {
-    return (
-        <div className="px-4">
+  return (
+    <div className="px-4">
       <div
         className={`md:w-1/2 md:min-w-[650px] mx-auto  p-8 mt-10 rounded-lg bg-white`}
       >
@@ -51,47 +51,40 @@ const SellerRequest = () => {
             />
           </div>
 
-          <div className='grid grid-cols-1 md:grid-cols-2 md:gap-5'>
-            
-          <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
-            <label htmlFor="phone_Number" className="text-xl font-medium">
-              Phone Number
-            </label>
-            <input
-              type="number"
-              placeholder="Enter Your Phone Number"
-              required
-              name="phone_Number"
-              className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
-            />
-          </div>
-
-          <div>
-          <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
-              <label htmlFor="email" className="text-xl font-medium">
-                Upload Your Photo 
+          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
+            <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
+              <label htmlFor="phone_Number" className="text-xl font-medium">
+                Phone Number
               </label>
               <input
-                type="file"
-                placeholder=""
+                type="number"
+                placeholder="Enter Your Phone Number"
                 required
-                name="photo"
-                className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                name="phone_Number"
+                className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
               />
             </div>
 
+            <div>
+              <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
+                <label htmlFor="email" className="text-xl font-medium">
+                  Upload Your Photo
+                </label>
+                <input
+                  type="file"
+                  placeholder=""
+                  required
+                  name="photo"
+                  className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                />
+              </div>
+            </div>
           </div>
-
-          
-
-          </div>
-
-
 
           <div className="grid grid-cols-1 md:grid-cols-2 md:gap-5">
             <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
               <label htmlFor="email" className="text-xl font-medium">
-                NID Card Photo 
+                NID Card Photo
               </label>
               <input
                 type="file"
@@ -102,22 +95,19 @@ const SellerRequest = () => {
               />
             </div>
             <div className=" rounded-lg flex flex-col gap-2 mb-4  ">
-            <label htmlFor="email" className="text-xl font-medium">
-              NID Number
-            </label>
-            <input
-              type="number"
-              placeholder="Enter Your NID Number"
-              required
-              name="nid_Number"
-              className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
-            />
-          </div>
+              <label htmlFor="email" className="text-xl font-medium">
+                NID Number
+              </label>
+              <input
+                type="number"
+                placeholder="Enter Your NID Number"
+                required
+                name="nid_Number"
+                className="flex-auto p-4 block rounded-lg font-medium outline-none border border-transparent border-stone-600 focus:border-primary focus:text-black"
+              />
+            </div>
           </div>
 
-          
-
-        
           <div className=" rounded-lg flex flex-col gap-2 mb-4 mt-3 ">
             <label htmlFor="email" className="text-xl font-medium">
               Write Something About You_
@@ -132,13 +122,13 @@ const SellerRequest = () => {
 
           <div className="flex justify-center mt-5 pb-5">
             <button className="text-center w-full border border-primary hover:border-primary hover:bg-transparent hover:text-primary  px-10 bg-primary text-white transition-all py-2 text-xl font-medium rounded">
-              Submit Here
+              Add Members
             </button>
           </div>
         </form>
       </div>
     </div>
-    );
+  );
 };
 
 export default SellerRequest;
