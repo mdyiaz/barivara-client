be59e6c2ade912d7096e57ab9f087dcf231e78a7
mdyiaz <mdyiaz@gmail.com>
2023-02-25 19:42:49 +0600

    getting-booking-data

--- src/component/Dashbord/BookingForSeller.jsx
+++ src/component/Dashbord/BookingForSeller.jsx
@@ -1,9 +1,80 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 const BookingForSeller = () => {
+
+
+
+
+  const { data: bookings = [] } = useQuery({
+    queryKey: ["bookings"],
+    queryFn: async () => {
+      const res = await fetch("http://localhost:5000/booking");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+
   return (
     <div>
-      <h1>Booking for seller</h1>
+       <div className="overflow-x-auto w-full">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>
+                <label>
+                  <input type="checkbox" className="checkbox" />
+                </label>
+              </th>
+              <th>House Name & Image</th>
+              <th>Buyer Name & Email</th>
+              <th>Buyer Number</th>
+              <th>Buyer Transaction ID</th>
+              <th>Buyer Payment Number</th>
+              <th>Buyer Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bookings.map((booking, i) => (
+              <tr key={booking._id}>
+                <th>
+                  <label>{i + 1}</label>
+                </th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        <img
+                          src={booking?.housePhoto}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{booking?.houseTitle}</div>
+                      {/* <div className="text-sm opacity-50">{home?.price}</div> */}
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {booking?.customerName}
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    {booking?.customerEmail}
+                  </span>
+                </td>
+                <td>{booking?.customerPhone}</td>
+                <td>{booking?.customerTransactionId}</td>
+                <td>{booking?.customerPaymentNumber}</td>
+                <td>{booking?.customerMessage}</td>
+                
+                
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
