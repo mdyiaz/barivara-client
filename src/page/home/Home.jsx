77bb26e6807b50a356bbf019102c47b2777ddaa8
mdyiaz <mdyiaz@gmail.com>
2023-02-25 16:15:49 +0600

    delete-photogallery-component

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HomeCard from "../../component/HomeCard/HomeCard";
 import OurCategory from "../../component/OurCategory/OurCategory";
-import PhotoGellary from "../../component/PhotoGellary/PhotoGellary";
+
 import Slider from "../../component/Slider/Slider";
 import Testimonial from "../../component/Testimonial/Testimonial";
 import Search from "./Search/Search";
@@ -13,7 +13,7 @@ function Home() {
       <Search></Search>
       <OurCategory></OurCategory>
       <HomeCard></HomeCard>
-      <PhotoGellary></PhotoGellary>
+      
       <Testimonial></Testimonial>
     </div>
   );
